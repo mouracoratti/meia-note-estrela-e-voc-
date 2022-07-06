653aa6882be1919b79ba4ddd23c9bbbf19ac4f5f
mouracoratti <105809199+mouracoratti@users.noreply.github.com>
2022-07-06 10:13:51 -0300

    Create estrelas.java

---
+++ estrelas.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class geo(){
+  static Scanner v = new Scanner(System.in);
+  
+ public static void main(String[]args){
+   System.out.println("voce digitou :" + v);
+ }
+}
